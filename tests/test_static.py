82b0ff56c55e97a92bcca6f435853695eae0e274
Riccardo Zanella <riccardo.zanella@injenia.it>
2021-04-26 23:05:04 +0200

    adding basic tests

--- tests/test_static.py
+++ tests/test_static.py
@@ -1,49 +1,103 @@
+import configparser
+import os
 import pathlib
 import pytest
+import json
 from cookiecutter.main import cookiecutter
 
 TEMPLATE_DIRECTORY = str(pathlib.Path(__file__).parent.parent)
+LINTER_FILE = '.flake8'
 
 
-@pytest.fixture()
-def create_cookie(scope='module'):
+@pytest.fixture(scope='session')
+def default_values():
+    with open('cookiecutter.json') as f:
+        return json.load(f)
+
+
+@pytest.fixture(scope='session')
+def create_cookie(tmpdir_factory):
     """
         cookiecutter run
     """
 
-    cookiedir = 'TESTDIR'
-    # cookiedir = str(tmpdir_factory.mktemp('cookie'))
+    cookiedir = str(tmpdir_factory.mktemp('cookie'))
     cookiecutter(
         template=TEMPLATE_DIRECTORY,
         output_dir=cookiedir,
         no_input=True,
     )
 
-    yield cookiedir
+    return cookiedir
 
 
-def test_folder_structure(create_cookie):
-    """
-        project structure
-    """
-    pass
+@pytest.fixture(scope='session')
+def content_list(create_cookie):
+
+    def remove_cookie_dir(text: str, prefix) -> str:
+        out = text
+        if text.startswith(prefix):
+            out = text[len(prefix):]
+        return out
+
+    content = []
+    for dirpath, dirnames, filenames in os.walk(create_cookie,
+                                                topdown=False,
+                                                followlinks=True):
+        for name in filenames:
+            fullname = os.path.join(dirpath, name)
+            content.append(fullname)
+        for name in dirnames:
+            fullname = os.path.join(dirpath, name)
+            content.append(fullname)
+
+    # remove leading cookie_dir
+    cookie_dir = os.path.join(create_cookie, '')
+    content = [
+        f for f in map(lambda x: remove_cookie_dir(x,  cookie_dir),
+                       content)
+    ]
+
+    return content
 
 
-def test_venv_valid(create_cookie):
-    """
-    """
-    pass
+project_slug = 'project_slug'
 
 
-def test_venv_ignored_in_linter(create_cookie):
-    """
-        virtualenv in flake8 file
-    """
-    pass
+def test_default_root_folder(content_list, default_values):
+    assert default_values['project_slug'] in content_list
 
 
-def test_venv_ignored_in_git(create_cookie):
-    """
-        virtualenv in .gitignore file
-    """
-    pass
+def test_default_package_folder(content_list, default_values):
+    assert os.path.join(
+        default_values['project_slug'],
+        default_values['package_name']
+    ) in content_list
+
+
+def test_default_venv_folder(content_list, default_values):
+    interpreters = [
+        os.path.join(
+            default_values['project_slug'],
+            default_values['virtualenv_name'],
+            'bin',
+            ii
+        ) for ii in ['python', 'python3']
+    ]
+
+    for item in interpreters:
+        assert item in content_list
+
+
+def test_venv_ignored_in_linter(create_cookie, default_values):
+    config = configparser.ConfigParser()
+    config.read(os.path.join(create_cookie,
+                             default_values['project_slug'],
+                             LINTER_FILE))
+
+    assert default_values['virtualenv_name'] in config['flake8']['exclude']
+
+
+def test_venv_ignored_in_gitignore(content_list, default_values):
+    lines = open('.gitignore').read().split('\n')
+    assert default_values['virtualenv_name'] in lines
