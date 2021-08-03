8f907836cc674280707f99c265df5609c07a71bc
Riccardo Zanella <riccardo.zanella@gmail.com>
2021-08-03 17:38:57 +0200

    using black to test formatting

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -8,13 +8,14 @@ import copy
 from typing import List, Type, TypeVar
 from pathlib import Path
 
+
 def str2bool(var: str):
     """
     String to Boolean
 
     :param var: input string
     """
-    return var.lower() in ("yes", 'true', 'si', 'y', 's', '1')
+    return var.lower() in ("yes", "true", "si", "y", "s", "1")
 
 
 class ExtendedEnvBuilder(venv.EnvBuilder):
@@ -23,6 +24,7 @@ class ExtendedEnvBuilder(venv.EnvBuilder):
 
     from: https://github.com/LP-CDF/AMi_Image_Analysis/blob/master/Setup.py
     """
+
     def __init__(self, *args, **kwargs):
         """
         Initialization
@@ -31,10 +33,11 @@ class ExtendedEnvBuilder(venv.EnvBuilder):
         :param upgrade_pip: whether to upgrade pip [default: True]
         :param ipython_support: whether to add ipython support [default: False]
         """
-        self.requirements_files = kwargs.pop('requirements_files',
-                                             ['requirements.txt'])
-        self.upgrade_pip = kwargs.pop('upgrade_pip', True)
-        self.ipython_support = kwargs.pop('ipython_support', False)
+        self.requirements_files = kwargs.pop(
+            "requirements_files", ["requirements.txt"]
+        )
+        self.upgrade_pip = kwargs.pop("upgrade_pip", True)
+        self.ipython_support = kwargs.pop("ipython_support", False)
         super().__init__(*args, **kwargs)
 
     def post_setup(self, context):
@@ -44,7 +47,7 @@ class ExtendedEnvBuilder(venv.EnvBuilder):
         :param context: The information for the virtual environment
                         creation request being processed.
         """
-        os.environ['VIRTUAL_ENV'] = context.env_dir
+        os.environ["VIRTUAL_ENV"] = context.env_dir
 
         self.install_dep(context)
 
@@ -63,23 +66,42 @@ class ExtendedEnvBuilder(venv.EnvBuilder):
         # upgrade pip
         if self.upgrade_pip:
             subprocess.check_call(
-                [binpath, '-m', 'pip', 'install', "--prefix",
-                    context.env_dir, '--upgrade', 'pip']
+                [
+                    binpath,
+                    "-m",
+                    "pip",
+                    "install",
+                    "--prefix",
+                    context.env_dir,
+                    "--upgrade",
+                    "pip",
+                ]
             )
 
         for req in self.requirements_files:
             subprocess.check_call(
-                [binpath, '-m', 'pip', 'install', "--prefix",
-                 context.env_dir, '-r', req]
+                [
+                    binpath,
+                    "-m",
+                    "pip",
+                    "install",
+                    "--prefix",
+                    context.env_dir,
+                    "-r",
+                    req,
+                ]
             )
 
         if self.ipython_support:
+            subprocess.check_call([binpath, "-m", "pip", "install", "notebook"])
             subprocess.check_call(
-                [binpath, '-m', 'pip', 'install', 'notebook']
-            )
-            subprocess.check_call(
-                [ipythonpath, 'kernel', 'install',
-                '--user', '--name='+context.env_name]
+                [
+                    ipythonpath,
+                    "kernel",
+                    "install",
+                    "--user",
+                    "--name=" + context.env_name,
+                ]
             )
 
 
@@ -91,8 +113,7 @@ def venv_create(venv_home: str, **kwargs):
     builder.create(venv_home)
 
 
-def gitignore_config(gitignore_template: str,
-                     ignore_list: List[str]):
+def gitignore_config(gitignore_template: str, ignore_list: List[str]):
     """
     Configure gitignore file
 
@@ -100,31 +121,29 @@ def gitignore_config(gitignore_template: str,
     :param ignore_list: list of gitignore entries
 
     """
-    gitignore_file = '.gitignore'
+    gitignore_file = ".gitignore"
 
-    os.rename(gitignore_template,
-              gitignore_file)
+    os.rename(gitignore_template, gitignore_file)
 
     # ignore list to .gitignore
-    with open(gitignore_file, 'a') as fd:
+    with open(gitignore_file, "a") as fd:
         for item in ignore_list:
-            fd.write('{}\n'.format(item))
+            fd.write("{}\n".format(item))
 
 
-def configure_version_control(branch_name='main',
-                              msg='first commit'):
+def configure_version_control(branch_name="main", msg="first commit"):
     """
-        Configure git version control
+    Configure git version control
 
-        :param branch_name: name for the branch [default: main]
-        :param msg: message for first commit [default "first commit"]
+    :param branch_name: name for the branch [default: main]
+    :param msg: message for first commit [default "first commit"]
 
-        Initialize git, add files, commit on requested branch
+    Initialize git, add files, commit on requested branch
     """
-    subprocess.check_call(['git', 'init'])
-    subprocess.check_call(['git', 'add', '.'])
-    subprocess.check_call(['git', 'commit', '-m', msg])
-    subprocess.check_call(['git', 'branch', '-m', branch_name])
+    subprocess.check_call(["git", "init"])
+    subprocess.check_call(["git", "add", "."])
+    subprocess.check_call(["git", "commit", "-m", msg])
+    subprocess.check_call(["git", "branch", "-m", branch_name])
 
 
 class ToolConfig:
@@ -143,8 +162,8 @@ class ToolConfig:
         # dict containing tool settings
         self.vscode_config: dict = {}
 
-
     _T = TypeVar("_T")
+
     @classmethod
     def from_folder(cls: Type[_T], config_folder) -> _T:
         """
@@ -161,23 +180,26 @@ class ToolConfig:
         config = cls()
 
         config.tool_config_files = [
-            os.path.join(config_folder, 'config_files', item.name)
-            for item in os.scandir(os.path.join(config_folder, 'config_files')) if item.is_file()
+            os.path.join(config_folder, "config_files", item.name)
+            for item in os.scandir(os.path.join(config_folder, "config_files"))
+            if item.is_file()
         ]
-    
-        with open(os.path.join(config_folder,'requirements.txt'), "r") as f:
+
+        with open(os.path.join(config_folder, "requirements.txt"), "r") as f:
             config.requirements.extend(f.read().split())
 
         try:
 
-            vscode_config_file = os.path.join(config_folder, 'vscode_config.json')
+            vscode_config_file = os.path.join(
+                config_folder, "vscode_config.json"
+            )
             with open(vscode_config_file, "r") as f:
                 config.vscode_config = json.load(f)
-        
+
         except json.JSONDecodeError:
             if os.path.getsize(vscode_config_file) != 0:
                 raise
-    
+
         return config
 
     def __add__(self, other):
@@ -206,37 +228,38 @@ class ToolConfig:
 
     def __repr__(self) -> str:
 
-        out = '\n\ntool_config_files:\n'
+        out = "\n\ntool_config_files:\n"
         out += self.tool_config_files.__repr__()
-        out += '\n\nrequirements:\n'
+        out += "\n\nrequirements:\n"
         out += self.requirements.__repr__()
-        out += '\n\nvscode_config:\n'
+        out += "\n\nvscode_config:\n"
         out += self.vscode_config.__repr__()
 
         return out
 
+
 def do_post_hook():
 
-    gitignore_template = '.gitignore.template'
-    vscode_config_file = '.vscode/settings.json'
-    tools_config_folder = 'tools_configs'
+    gitignore_template = ".gitignore.template"
+    vscode_config_file = ".vscode/settings.json"
+    tools_config_folder = "tools_configs"
 
     # Jinja values
-    virtualenv_name = '{{cookiecutter.virtualenv_name}}'
-    do_configure_git = str2bool('{{cookiecutter.configure_version_control}}')
-    do_upgrade_pip = str2bool('{{cookiecutter.upgrade_pip}}')
-    do_support_ipython = str2bool('{{cookiecutter.ipython_support}}')
-    chosen_linter = '{{cookiecutter.linter}}'
-    chosen_formatter = '{{cookiecutter.formatter}}'
+    virtualenv_name = "{{cookiecutter.virtualenv_name}}"
+    do_configure_git = str2bool("{{cookiecutter.configure_version_control}}")
+    do_upgrade_pip = str2bool("{{cookiecutter.upgrade_pip}}")
+    do_support_ipython = str2bool("{{cookiecutter.ipython_support}}")
+    chosen_linter = "{{cookiecutter.linter}}"
+    chosen_formatter = "{{cookiecutter.formatter}}"
 
     supported_tools = [
         item.name for item in os.scandir(tools_config_folder) if item.is_dir()
     ]
 
     tools_list = [chosen_linter, chosen_formatter]
-    tools_list = list(filter(lambda x: x.lower() not in ['', 'no'], tools_list))
+    tools_list = list(filter(lambda x: x.lower() not in ["", "no"], tools_list))
 
-    print('Running post-gen hook')
+    print("Running post-gen hook")
     # initialize base configuration
     master_cfg = ToolConfig()
     with open(vscode_config_file, "r") as f:
@@ -244,60 +267,62 @@ def do_post_hook():
 
     # collect configurations from tools
     try:
-        print('{}: configuration tools collection'.format(__name__))
+        print("{}: configuration tools collection".format(__name__))
 
         for tool in tools_list:
-            print('{}: configure {}'.format(__name__, tool))
+            print("{}: configure {}".format(__name__, tool))
 
             if tool not in supported_tools:
-                raise Exception('{} tool not supported'.format(tool))
+                raise Exception("{} tool not supported".format(tool))
 
             # get configuration
-            cfg = ToolConfig.from_folder(os.path.join(tools_config_folder, tool))
+            cfg = ToolConfig.from_folder(
+                os.path.join(tools_config_folder, tool)
+            )
 
             master_cfg += cfg
 
     except Exception as e:
-        print('{}: exception caught\nexiting\n{}'.format(__name__, e))
+        print("{}: exception caught\nexiting\n{}".format(__name__, e))
         sys.exit(-1)
 
-    print('{}: configuration'.format(__name__))
-    print('{}:{}'.format(__name__, master_cfg))
+    print("{}: configuration".format(__name__))
+    print("{}:{}".format(__name__, master_cfg))
 
     try:
         ### CONFIGURATION FILES
         for item in master_cfg.tool_config_files:
-            shutil.copy2(item, './')
+            shutil.copy2(item, "./")
 
         ### PREREQUISITES SETUP
-        with open('requirements-test.txt', 'a') as fd:
-            fd.write('\n'.join(master_cfg.requirements))
+        with open("requirements-test.txt", "a") as fd:
+            fd.write("\n".join(master_cfg.requirements))
 
         ### VIRTUALENV
-        print('{}: create virtualenv'.format(__name__))
-        venv_create(venv_home=virtualenv_name,
-                    requirements_files=[
-                        'requirements.txt','requirements-test.txt'
-                    ],
-                    upgrade_pip=do_upgrade_pip,
-                    ipython_support=do_support_ipython)
-        
+        print("{}: create virtualenv".format(__name__))
+        venv_create(
+            venv_home=virtualenv_name,
+            requirements_files=["requirements.txt", "requirements-test.txt"],
+            upgrade_pip=do_upgrade_pip,
+            ipython_support=do_support_ipython,
+        )
+
         ### GITIGNORE
-        print('{}: configure gitignore'.format(__name__))
+        print("{}: configure gitignore".format(__name__))
         gitignore_config(gitignore_template, [virtualenv_name])
 
         ### VSCODE CONFIGURATION
-        print('{}: configure vscode'.format(__name__))
-        with open(vscode_config_file, 'w') as f:
+        print("{}: configure vscode".format(__name__))
+        with open(vscode_config_file, "w") as f:
             json.dump(master_cfg.vscode_config, f, indent=4)
 
         # ### VERSION CONTROL SETTINGS
         if do_configure_git:
-            print('{}: configure git'.format(__name__))
+            print("{}: configure git".format(__name__))
             configure_version_control()
 
     except Exception as e:
-        print('{}: exception caught\nexiting\n{}'.format(__name__, e))
+        print("{}: exception caught\nexiting\n{}".format(__name__, e))
         sys.exit(-1)
 
 
--- tests/test_static.py
+++ tests/test_static.py
@@ -5,22 +5,22 @@ import pytest
 from cookiecutter.main import cookiecutter
 
 TEMPLATE_DIRECTORY = str(pathlib.Path(__file__).parent.parent)
-GITIGNORE = '.gitignore'
+GITIGNORE = ".gitignore"
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def default_values():
-    with open('cookiecutter.json') as f:
+    with open("cookiecutter.json") as f:
         return json.load(f)
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def create_cookie(tmpdir_factory):
     """
-        cookiecutter run
+    cookiecutter run
     """
 
-    cookiedir = str(tmpdir_factory.mktemp('cookie'))
+    cookiedir = str(tmpdir_factory.mktemp("cookie"))
     cookiecutter(
         template=TEMPLATE_DIRECTORY,
         output_dir=cookiedir,
@@ -30,19 +30,18 @@ def create_cookie(tmpdir_factory):
     return cookiedir
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def content_list(create_cookie):
-
     def remove_cookie_dir(text: str, prefix) -> str:
         out = text
         if text.startswith(prefix):
-            out = text[len(prefix):]
+            out = text[len(prefix) :]
         return out
 
     content = []
-    for dirpath, dirnames, filenames in os.walk(create_cookie,
-                                                topdown=False,
-                                                followlinks=True):
+    for dirpath, dirnames, filenames in os.walk(
+        create_cookie, topdown=False, followlinks=True
+    ):
         for name in filenames:
             fullname = os.path.join(dirpath, name)
             content.append(fullname)
@@ -51,60 +50,66 @@ def content_list(create_cookie):
             content.append(fullname)
 
     # remove leading cookie_dir
-    cookie_dir = os.path.join(create_cookie, '')
+    cookie_dir = os.path.join(create_cookie, "")
     content = [
-        f for f in map(lambda x: remove_cookie_dir(x,  cookie_dir),
-                       content)
+        f for f in map(lambda x: remove_cookie_dir(x, cookie_dir), content)
     ]
 
     return content
 
 
 def test_default_root_folder(content_list, default_values):
-    assert default_values['project_slug'] in content_list
+    assert default_values["project_slug"] in content_list
 
 
 def test_default_package_folder(content_list, default_values):
-    assert os.path.join(
-        default_values['project_slug'],
-        default_values['package_name']
-    ) in content_list
+    assert (
+        os.path.join(
+            default_values["project_slug"], default_values["package_name"]
+        )
+        in content_list
+    )
 
 
 def test_default_pip_in_venv_folder(content_list, default_values):
     interpreters = [
         os.path.join(
-            default_values['project_slug'],
-            default_values['virtualenv_name'],
-            'bin',
-            ii
-        ) for ii in ['pip', 'pip3']
+            default_values["project_slug"],
+            default_values["virtualenv_name"],
+            "bin",
+            ii,
+        )
+        for ii in ["pip", "pip3"]
     ]
 
     for item in interpreters:
         assert item in content_list
+
 
 def test_default_interpreter_in_venv_folder(content_list, default_values):
     interpreters = [
         os.path.join(
-            default_values['project_slug'],
-            default_values['virtualenv_name'],
-            'bin',
-            ii
-        ) for ii in ['python', 'python3']
+            default_values["project_slug"],
+            default_values["virtualenv_name"],
+            "bin",
+            ii,
+        )
+        for ii in ["python", "python3"]
     ]
 
     for item in interpreters:
         assert item in content_list
 
+
 def test_default_test_tools_in_venv_folder(content_list, default_values):
     interpreters = [
         os.path.join(
-            default_values['project_slug'],
-            default_values['virtualenv_name'],
-            'bin',
-            ii
-        ) for ii in ['pytest']
+            default_values["project_slug"],
+            default_values["virtualenv_name"],
+            "bin",
+            ii,
+        )
+        for ii in ["pytest"]
     ]
 
     for item in interpreters:
@@ -122,8 +127,8 @@ def test_default_test_tools_in_venv_folder(content_list, default_values):
 
 
 def test_venv_ignored_in_gitignore(create_cookie, default_values):
-    with open(os.path.join(create_cookie,
-                           default_values['project_slug'],
-                           GITIGNORE)) as f:
-        lines = f.read().split('\n')
-    assert default_values['virtualenv_name'] in lines
+    with open(
+        os.path.join(create_cookie, default_values["project_slug"], GITIGNORE)
+    ) as f:
+        lines = f.read().split("\n")
+    assert default_values["virtualenv_name"] in lines
